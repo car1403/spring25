e3b475718f2ab0397fafb17162921ce0ba3f947b
jeanm <jmlee@tonesol.com>
2025-03-07 13:32:24 +0900

    2025.3.7 TEST2

--- day02/src/main/java/com/mc/controller/MainController.java
+++ day02/src/main/java/com/mc/controller/MainController.java
@@ -1,14 +1,25 @@
 package com.mc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
-    public String main(){
+    public String main(Model model){
+        String str = "James";
+        int age = 20;
+        model.addAttribute("name",str);
+        model.addAttribute("age",age);
         return "index";
+    }
+
+    @RequestMapping("/next")
+    public String next(Model model){
+
+        return "next";
     }
 
 }
