8628260c031204d7ee3b02574ad23d620b7bd331
jeanm <jmlee@tonesol.com>
2025-03-07 15:11:05 +0900

    2025.3.7 TEST2

--- day02ws1/src/main/java/com/mc/controller/MainController.java
+++ day02ws1/src/main/java/com/mc/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.mc.controller;
 
-import com.mc.dto.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
