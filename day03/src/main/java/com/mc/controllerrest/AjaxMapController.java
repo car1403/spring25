9995391be9d987445dcbf86b3cff0af8f268a4cf
jeanm <jmlee@tonesol.com>
2025-03-14 16:22:48 +0900

    2025.3.13

--- day03/src/main/java/com/mc/controllerrest/AjaxMapController.java
+++ day03/src/main/java/com/mc/controllerrest/AjaxMapController.java
@@ -33,4 +33,29 @@ public class AjaxMapController {
         return contents;
     }
 
+    @RequestMapping("/getcontents")
+    public Object getcontents(@RequestParam("target") int target, @RequestParam("type") int type){
+        log.info(target+" "+type);
+        List<Content> contents = new ArrayList<>();
+        if(target == 100){
+            if(type == 10){
+                contents.add(new Content(37.564472,126.990841,"순대국1", "ss1.jpg", 101));
+                contents.add(new Content(37.544472,126.970841,"순대국2", "ss2.jpg", 102));
+                contents.add(new Content(37.564472,126.970841,"순대국3", "ss3.jpg", 103));
+
+            }else if(type == 20){
+                contents.add(new Content(37.554472,126.910841,"순1", "ss1.jpg", 101));
+                contents.add(new Content(37.514472,126.920841,"순2", "ss2.jpg", 102));
+                contents.add(new Content(37.534472,126.990841,"순3", "ss3.jpg", 103));
+
+            }else if(type == 30){
+                contents.add(new Content(37.574472,126.920841,"순대국1", "ss1.jpg", 101));
+                contents.add(new Content(37.584472,126.970841,"순대국2", "ss2.jpg", 102));
+                contents.add(new Content(37.514472,126.930841,"순대국3", "ss3.jpg", 103));
+
+            }
+          }
+        return contents;
+    }
+
 }
