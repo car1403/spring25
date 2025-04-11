86abc5863dd33735ce53748bf2f4aa70f0f9d6af
jeanm <jmlee@tonesol.com>
2025-04-11 09:36:24 +0900

    2025.4.11

---
+++ admin/src/main/java/com/mc/app/msg/AdminMsg.java
@@ -0,0 +1,15 @@
+package com.mc.app.msg;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class AdminMsg {
+    private int content1;
+    private int content2;
+    private int content3;
+    private int content4;
+
+}
--- admin/src/main/java/com/mc/config/StomWebSocketConfig.java
+++ admin/src/main/java/com/mc/config/StomWebSocketConfig.java
@@ -13,7 +13,7 @@ public class StomWebSocketConfig implements WebSocketMessageBrokerConfigurer{
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
-//        registry.addEndpoint("/wss").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/wss").setAllowedOriginPatterns("*").withSockJS();
 //        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
     }
     @Override
